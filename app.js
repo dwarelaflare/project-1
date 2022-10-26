647d3d048d76c12e2293758916804777d235b4e3
dwarelaflare <78941609+dwarelaflare@users.noreply.github.com>
2022-10-25 20:50:26 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -6,15 +6,28 @@
 //WHAT IS THE BEST TYPEFORM API 
 
 // variable for api key
-const apiKey = "1efba983fdc68bd9a381566971642982"
+//const apiKey = "1efba983fdc68bd9a381566971642982"
 
 // variable for base url
-const baseURL = "https://api.crunchbase.com/api/v4/searches/organizations"
+//const baseURL = "https://api.crunchbase.com/api/v4/searches/organizations"
 
 // function that does movie search
-function companySearch(identifier){
-    const url = `${baseURL}?t=${identifier}&apikey=${apiKey}`
-    console.log(url)
-}
+//function companySearch(identifier){
+    //const url = `${baseURL}?t=${identifier}&apikey=${apiKey}`
+    //console.log(url)
+//}
 
-companySearch("breakr")
+//companySearch("breakr")
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0f424e29d9mshf95ab5dc2691c8ap1be280jsn3e0cb32ce82e',
+		'X-RapidAPI-Host': 'crunchbase-crunchbase-v1.p.rapidapi.com'
+	}
+};
+
+fetch('https://crunchbase-crunchbase-v1.p.rapidapi.com/autocompletes?query=breakr', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
